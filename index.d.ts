57044f7b96df6ad21c48d94ad45677550f4a5c1b
[1000i100] Millicent Billette <git@1000i100.fr>
2022-12-06 14:09:54 +0100

    update type definition with function parameters and shortUnits

--- index.d.ts
+++ index.d.ts
@@ -1,18 +1,43 @@
-export type TimeComponents = {
-	millennia: number;
-	centuries: number;
-	years: number;
-	months: number;
-	weeks: number;
-	days: number;
-	hours: number;
-	minutes: number;
-	seconds: number;
-	milliseconds: number;
-	microseconds: number;
-	nanoseconds: number;
-	picoseconds: number;
+export type TimeComponentsLong = {
+	millennia?: number;
+	centuries?: number;
+	years?: number;
+	months?: number;
+	weeks?: number;
+	days?: number;
+	hours?: number;
+	minutes?: number;
+	seconds?: number;
+	milliseconds?: number;
+	microseconds?: number;
+	nanoseconds?: number;
+	picoseconds?: number;
 };
+export type TimeComponentsShort = {
+  ky?: number;
+  c?: number;
+  Y?: number;
+  M?: number;
+  W?: number;
+  D?: number;
+  h?: number;
+  m?: number;
+  s?: number;
+  ms?: number;
+  µs?: number;
+  ns?: number;
+  ps?: number;
+};
+export type TimeComponents = Merge<TimeComponentsShort|TimeComponentsLong>;
+
+// https://dev.to/lucianbc/union-type-merging-in-typescript-9al
+type Merge<T extends object> = {
+  [k in AllKeys<T>]: PickType<T, k>;
+};
+type AllKeys<T> = T extends any ? keyof T : never;
+type PickType<T, K extends AllKeys<T>> = T extends { [k in K]?: any }
+	? T[K]
+	: undefined;
 
 /**
 Parse milliseconds into an object.
@@ -33,4 +58,37 @@ parseMilliseconds(1337000001);
 // }
 ```
  */
-export default function parseMilliseconds(milliseconds: number): TimeComponents;
+export default function parseMilliseconds(milliseconds: number, options?: parseMillisecondsOptions): TimeComponents;
+export type parseMillisecondsOptions = {
+  upToUnit?: TimeUnits;// keyof typeof TimeUnits;
+  downToUnit?: TimeUnits;
+  outputFormat?: 'short' | 'long' | 'both';
+};
+export enum TimeUnits {
+  millennia = "millennia",
+  centuries = "centuries",
+  years = "years",
+  months = "months",
+  weeks = "weeks",
+  days = "days",
+  hours = "hours",
+  minutes = "minutes",
+  seconds = "seconds",
+  milliseconds = "milliseconds",
+  microseconds = "microseconds",
+  nanoseconds = "nanoseconds",
+  picoseconds = "picoseconds",
+  ky = "ky",
+  c = "c",
+  Y = "Y",
+  M = "M",
+  W = "W",
+  D = "D",
+  h = "h",
+  m = "m",
+  s = "s",
+  ms = "ms",
+  µs = "µs",
+  ns = "ns",
+  ps = "ps",
+}
